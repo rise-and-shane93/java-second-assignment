24c0215c47eb70b69d37279ce7b761ebc2987263
Shane Harper <cdtharper@gmail.com>
2021-02-06 11:42:20 -0500

    modified output to include sums of the user inputs by row and column

--- Original.java
+++ Original.java
@@ -11,6 +11,9 @@ public class Original
     int v4 = 0;      // Fourth value to be input
     int v5 = 0;      // Fifth value to be input
     int v6 = 0;      // Sixth value to be input
+
+    // horizontal sums of inputs
+    int v7, v8, v9;
     
     // Use a Scanner to input integer values
     Scanner input = new Scanner( System.in );
@@ -22,13 +25,31 @@ public class Original
     v4 = input.nextInt();     // Input fourth value
     v5 = input.nextInt();     // Input fifth value
     v6 = input.nextInt();     // Input sixth value
+
+    v7 = v1 + v2; // Sum of first and second inputted values
+    v8 = v3 + v4; // Sum of third and fourth inputted values
+    v9 = v5 + v6; // Sum of fifth and sixth inputted values
     
     // Output using System.out.println()
     System.out.println( "\n\n" );
-    System.out.println( "\t" + "Value" + "\t" + "Value" );
-    System.out.println( "\t" + v1 + "\t" + v2 );
-    System.out.println( "\t" + v3 + "\t" + v4 );
-    System.out.println( "\t" + v5 + "\t" + v6 );
+
+    // Print out the table headers with a tab space in between
+    System.out.println( "\t" + "Value" + "\t" + "Value" + "\t" + "Total" );
+
+    /* Output three numbers per row (3 rows total): two of the values
+    that the user inputted followed by the variable corresponding
+    to the sum of the previous two user-inputted values */
+    System.out.println( "\t" + v1 + "\t" + v2 + "\t" + v7 );
+    System.out.println( "\t" + v3 + "\t" + v4 + "\t" + v8 );
+    System.out.println( "\t" + v5 + "\t" + v6 + "\t" + v9 );
+
+    /*Print out a three sets of hyphens with a tab space in between
+    This is for the vertical sum of each column */
+    System.out.println( "\t" + "---" + "\t" + "---" + "\t" + "---" );
+
+    // Print the vertical sum of each columns
+    System.out.println( "Total" + "\t" + (v1+v3+v5) + "\t" + (v2+v4+v6) + "\t" + (v7+v8+v9) );
+
     System.out.println( "\n\n" );
   }
 }
